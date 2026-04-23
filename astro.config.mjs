19e1c5b547b4f3da76c86cecb17daf940fab0003
Jhoel RN <62358739+JhoelRN@users.noreply.github.com>
2026-04-22 19:08:19 -0500

    configuracion for public page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
 import { defineConfig } from 'astro/config';
+import { fileURLToPath } from 'node:url';
 
 export default defineConfig({
-  site: 'https://dtpautomation.local'
+  site: 'https://jhoelrn.github.io',
+  base: '/datapro-automation',
+  vite: {
+    resolve: {
+      alias: {
+        'astro/entrypoints/prerender': fileURLToPath(new URL('./node_modules/astro/dist/entrypoints/prerender.js', import.meta.url))
+      }
+    }
+  }
 });
